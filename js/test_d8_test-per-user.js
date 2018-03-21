45bc7bcaa5ab1e7e9388235320a65804db6ac044
Nono95230 <arnaud.bach1987@gmail.com>
2018-03-21 17:20:47 +0100

    [MERGE CODE] - Add Chris code
    Add Many Changes for the Dashboard

--- js/test_d8_test-per-user.js
+++ js/test_d8_test-per-user.js
@@ -7,122 +7,69 @@
                 return decodeURIComponent(window.location.search.replace(new RegExp("^(?:.*[&\\?]" + encodeURIComponent(key).replace(/[\.\+\*]/g, "\\$&") + "(?:\\=([^&]*))?)?.*$", "i"), "$1"));
             }
 
-            var render = new Array(),
-                index,
-                id,
-                rows = drupalSettings.TestD8.chart.rows,
-                disabledTabs = new Array();
-
-            for (index in rows){
-                var tmp = rows[index];
-                for (id in tmp){
-
-                    // désactive les onglets sans données
-                    if (tmp[id].length == 0){
-                        disabledTabs.push(Number(index));
-                    } else {
-
-
-                    }
-                }
-            }
-
-            // active l'onglet du theme du test fini
-            var tabId = getQueryStringValue('tab'),
-                activeTab = 0;
-            if (tabId){
-                dance:
-                for (index in rows){
-                    activeTab = index;
-                    for (id in rows[index]){
-                        if (id == tabId){
-                            activeTab = index;
-                            break dance;
+            var drupalsettings_chart = drupalSettings.TestD8.chart.data,
+                num_questions = drupalSettings.TestD8.chart.number_of_questions,
+                disabledTabs = [],
+                tabId = getQueryStringValue('tab'),
+                activeTab = 0,
+                myChart = [],
+                chartOptions = {
+                    legend: {
+                        display: false
+                    },
+                    tooltips: {
+                        callbacks: {
+                            label: function(tooltipItem) {
+                                return tooltipItem.yLabel;
+                            }
                         }
-                    }
-                }
-            }
+                    },
+                    responsive: true,
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true,
+                                max: num_questions,
+                                min: 0
+                            }
+                        }]
+                    },
+                    //title: { display: false, text: 'Résultats pour "Webmaster"' },
+                    tooltips: { mode: 'index', intersect: false }
+                    //hover: { mode: 'nearest', intersect: true }
+                };
 
-            $(function(){
-                $("#tabs").tabs({
-                    active: activeTab,
-                    disabled: disabledTabs,
-                    activate: function(event, ui){
-                        drawChart[ui.newTab.index()]();
-                    }
+            for (var index in drupalsettings_chart){
+                var value = drupalsettings_chart[index];
+
+                // active l'onglet du theme du test fini
+                if (tabId == value.id){ activeTab = index; }
+
+                // disable tabs with no data
+                if (value.num == 0){ disabledTabs.push(Number(index)); }
+
+                // implements drawChart functions
+                myChart[index] = new Chart($("#chart-" + value.id), {
+                    type: (value.num < 2 ? 'bar' : 'line'),
+                    data: {
+                        labels: drupalsettings_chart[index].chartLabels,
+                        datasets: [{
+                            data: drupalsettings_chart[index].chartData,
+                            lineTension: 0.2,
+                            label: '',
+                            fill: false,
+                            borderColor: "rgba(0, 120, 164, 0.75)",
+                            borderWidth: 2
+                        }]
+                    },
+                    options: chartOptions
                 });
+            }
+
+            $("#tabs").tabs({
+                active: activeTab,
+                disabled: disabledTabs
             });
-
-
-
-            var drawChart = [];
-            drawChart['0'] = function(){
-                var data = new google.visualization.DataTable();
-                data.addColumn('string');
-                data.addColumn('number');
-                data.addRows(drupalSettings.TestD8.chart.Webmaster);
-                var options = {
-                    legend: { position:'none' },
-                    chart: { title: 'Évolution de vos résultats pour le thème Webmaster' },
-                    width: "100%",
-                    height: 300,
-                    vAxis: { /*title: 'Scores',*/ viewWindow: {min:0, max:drupalSettings.TestD8.chart.number_of_questions} }
-                };
-                var chart = new google.charts.Line(document.getElementById('chart-1'));
-                chart.draw(data, google.charts.Line.convertOptions(options));
-            }
-            drawChart['1'] = function(){
-                var data = new google.visualization.DataTable();
-                data.addColumn('string');
-                data.addColumn('number');
-                data.addRows(drupalSettings.TestD8.chart.Webmaster);
-                var options = {
-                    legend: { position:'none' },
-                    chart: { title: 'Évolution de vos résultats pour le thème Themer' },
-                    width: "100%",
-                    height: 300,
-                    vAxis: { /*title: 'Scores',*/ viewWindow: {min:0, max:drupalSettings.TestD8.chart.number_of_questions} }
-                };
-                var chart = new google.charts.Line(document.getElementById('chart-2'));
-                chart.draw(data, google.charts.Line.convertOptions(options));
-            }
-            drawChart['2'] = function(){
-                var data = new google.visualization.DataTable();
-                data.addColumn('string');
-                data.addColumn('number');
-                data.addRows(drupalSettings.TestD8.chart.Webmaster);
-                var options = {
-                    legend: { position:'none' },
-                    chart: { title: 'Évolution de vos résultats pour le thème Développement' },
-                    width: "100%",
-                    height: 300,
-                    vAxis: { /*title: 'Scores',*/ viewWindow: {min:0, max:drupalSettings.TestD8.chart.number_of_questions} }
-                };
-                var chart = new google.charts.Line(document.getElementById('chart-3'));
-                chart.draw(data, google.charts.Line.convertOptions(options));
-            }
-            drawChart['3'] = function(){
-                var data = new google.visualization.DataTable();
-                data.addColumn('string');
-                data.addColumn('number');
-                data.addRows([
-                  ['10 déc 2014', 21],
-                  ['10 déc 2014', 21]
-                ]);
-                var options = {
-                    legend: { position:'none' },
-                    chart: { title: 'Évolution de vos résultats pour le thème Expert' },
-                    width: "100%",
-                    height: 300,
-                    vAxis: { /*title: 'Scores',*/ viewWindow: {min:0, max:drupalSettings.TestD8.chart.number_of_questions} }
-                };
-                var chart = new google.charts.Line(document.getElementById('chart-4'));
-                chart.draw(data, google.charts.Line.convertOptions(options));
-            }
-
-            google.charts.load('current', {'packages':['line']});
-            google.charts.setOnLoadCallback(drawChart[activeTab]);
-
 
         }
     };
